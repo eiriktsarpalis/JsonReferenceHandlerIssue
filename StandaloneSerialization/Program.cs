62e1c902e27d7b80a08ff10b41d4fbf34fa46c4c
Dan <codingdna@gmail.com>
2021-05-04 16:51:03 +0200

    Added StandaloneSerialization

---
+++ StandaloneSerialization/Program.cs
@@ -0,0 +1,39 @@
+﻿using JsonReferenceHandlerIssue;
+using JsonReferenceHandlerIssue.Controllers;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace StandaloneSerialization
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press ESC or CTRL-C to stop");
+
+            var stream = new MemoryStream();
+            
+            var options = new JsonSerializerOptions(JsonSerializerDefaults.Web) 
+            {
+                MaxDepth = 32,
+                ReferenceHandler = ReferenceHandler.Preserve,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
+            };
+
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+            {
+                WeatherForecast[] forecasts = new WeatherForecastController(null).Get().ToArray();
+
+                stream.Position = 0;
+
+                await JsonSerializer.SerializeAsync(stream, forecasts, options);
+            }
+
+            Console.WriteLine("Closing..");
+        }
+    }
+}
